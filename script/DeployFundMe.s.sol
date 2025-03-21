10a50e18268504eeea212fc6de859d0371e99438
yogitagoel <yogitagoel2006@gmail.com>
2025-03-21 10:45:14 +0530

    a

--- script/DeployFundMe.s.sol
+++ script/DeployFundMe.s.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.19;
 
 import {Script} from "../lib/forge-std/src/Script.sol";
-import {HelperConfig} from "./HelperConfig.s.sol";
+import {HelperConfig} from "../script/HelperConfig.s.sol";
 import {FundMe} from "../src/FundMe.sol";
 
 contract DeployFundMe is Script {
